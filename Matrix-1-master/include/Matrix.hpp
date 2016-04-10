db6e3f1815a15ba2ec4f1590c272dfc8f7a4e75b
rexxh <rexxh@mail.ru>
2016-04-10 18:13:17 +0300

    Update Matrix.hpp

--- Matrix-1-master/include/Matrix.hpp
+++ Matrix-1-master/include/Matrix.hpp
@@ -11,13 +11,46 @@ ofstream fout;
 ifstream fin;
 
 template <typename T>
+class Matrix;
+
+template <typename T>
+std::ostream & operator<<(std::ostream & output, const Matrix <T> & matrix)
+{
+	for (unsigned int i = 0; i < matrix.stroki; ++i) {
+		output << std::endl;
+		for (unsigned int j = 0; j < matrix.stolbs; ++j) {
+			output << matrix.e[i][j] << "\t";
+		}
+	}
+
+	return output;
+}
+
+template <typename T>
+std::istream & operator>>(std::istream & input, Matrix <T> & matrix)
+{
+	for (unsigned int i = 0; i < matrix.stroki; ++i) {
+		for (unsigned int j = 0; j < matrix.stolbs; ++j) {
+			if (!(input >> matrix.e[i][j])) {
+				throw "exception in fill matrix";
+			}
+		}
+	}
+
+	return input;
+}
+
+template <typename T>
 class Matrix
 {
 	int stroki;
 	int stolbs;
 	T **e;
-	void fill(T **e);
+	
 public:
+	friend std::ostream & operator<< <>(std::ostream & output, const Matrix &);
+	friend std::istream & operator>> <>(std::istream & input, Matrix &);
+
 	Matrix() : stroki(0), stolbs(0), e(nullptr) {};
 	Matrix(int n, int m) : stroki(n), stolbs(m)
 	{
@@ -26,11 +59,11 @@ public:
 			e[i] = new T[m];
 	};
 	Matrix(const Matrix &M) : stroki(M.stroki), stolbs(M.stolbs)
-	{ //Êîíñòðóêòîð êîïèðîâàíèÿ
+	{ //Конструктор копирования
 		e = new T*[stroki];
 		for (int i = 0; i < stroki; i++)
 		{
-			e[i] = new T [stolbs];
+			e[i] = new T[stolbs];
 		}
 		for (int i = 0; i < stroki; i++)
 		{
@@ -54,7 +87,7 @@ public:
 		}
 	};
 	void Set(char *path)
-	{ //Çàïèñü â ôàéë
+	{ //Запись в файл
 		fout.open(path);
 		for (int i = 0; i < stroki; i++)
 		{
@@ -70,7 +103,7 @@ public:
 	int STR() {  return stroki; };
 	int STOLB(){ return stolbs; };
 	int print()
-	{ //Âûâîä íà ýêðàí
+	{ //Вывод на экран
 		for (int i = 0; i < stroki; i++)
 		{
 			for (int j = 0; j < stolbs; j++)
@@ -83,17 +116,17 @@ public:
 	};
 
 	Matrix operator + ( const Matrix &M2)
-	{ //Îïåðàòîð ñëîæåíèÿ 2óõ ìàòðèö
+	{ //Оператор сложения 2ух матриц
 		Matrix M3(M2.stroki, M2.stolbs);
 		for (int i = 0; i < M2.stroki; i++)
 		for (int j = 0; j < M2.stolbs; j++)
 			M3.e[i][j] = e[i][j] + M2.e[i][j];
-		cout << "Ìàòðèöà 1 + Ìàòðèöà 2=\n"; M3.print();
+		cout << "Матрица 1 + Матрица 2=\n"; M3.print();
 		return M3;
 	};
 
 	Matrix operator * ( const Matrix &M2)
-	{ //Îïåðàòîð óìíîæåíèÿ 2óõ ìàòðèö
+	{ //Оператор умножения 2ух матриц
 		Matrix M3(stroki, M2.stolbs);
 		int k = 0;
 		for (int i = 0; i < stroki; i++)
@@ -107,46 +140,52 @@ public:
 				}
 			}
 		}
-		cout << "Ìàòðèöà 1 * Ìàòðèöà 2  =\n"; M3.print();
+		cout << "Матрица 1 * Матрица 2  =\n"; M3.print();
 		return M3;
 	}
 
 	T * operator [] (int k)
 	{
-		T* stroka = new T [stolbs];
+		double* stroka = new double[stolbs];
 		for (int j = 0; j < stolbs; j++)
 		{
 			stroka[j] = e[k - 1][j];
 		}
 		return stroka;
 	};
-	
-	void swap(Matrix & M2) 
-    {
-        // обмен всех членов с M2
-        std::swap(stroki, M2.stroki);
-        std::swap(stolbs, M2.stolbs);
-        std::swap(e, M2.e);
-    };
+
+	void swap(Matrix & M2)
+	{
+		// обмен всех членов с M2
+		std::swap(stroki, M2.stroki);
+		std::swap(stolbs, M2.stolbs);
+		std::swap(e, M2.e);
+	};
 
 	Matrix & operator = (Matrix M2)
 	{
-        // обмен this с M2
-	  swap(M2);
-          return *this;
-          //M2 уничтожается, освобождая память
+		// обмен this с M2
+		swap(M2);
+		return *this;
+		//M2 уничтожается, освобождая память
 	}
 
+	void filltest(char *path)
+	{
+		char* number;
+		fin.open(path);
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				fin >> e[i][j];
+				cout << e[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	
 };
 
-template <typename T>
-void Matrix<T>::fill(T **e)
-{
-    e = new T *[stroki];
-    for (unsigned int i = 0; i < stroki; ++i) {
-        e[i] = new T[stolbs];
-        for (unsigned int j = 0; j < stolbs; ++j) {
-            e[i][j] = e ? e[i][j] : 0;
-        }
-    }
-}
+#endif;
